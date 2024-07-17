99f2f12211e5df660e7dd7723477142da04a18f5
farzaneh <fatn@ug.kth.se>
2024-07-17 11:52:35 +0200

    changes for adding MolformerFinetuned

--- utils/visualization_helper.py
+++ utils/visualization_helper.py
@@ -160,7 +160,7 @@ def plot_bars(data, title, filename):
                # , "#4575b4"
                ]
 
-    palette = ['#4d79a4', '#ecc947', '#b07aa0']
+    palette = ['#4d79a4', '#ecc947', '#b07aa0','#103961']
 
     # plt.subplots_adjust(bottom=0.3)
     g = sns.barplot(df_corrs, x="Dataset", y="Correlation", hue="type", width=0.4, palette=palette)
@@ -212,10 +212,11 @@ def plot_bars(data, title, filename):
             # we recenter the bar
             patch.set_x(patch.get_x() + diff * .5)
 
-def combine_visualize(df1, df2, df3, tasks, ax, title, abs=False, figure_name="def"):
+def combine_visualize(df1,df4, df2, df3, tasks, ax, title, abs=False, figure_name="def"):
 
     df12 = pd.concat((df1, df2))
-    df_combined = pd.concat((df12, df3))
+    df124 = pd.concat((df12, df4))
+    df_combined = pd.concat((df124, df3))
     melted_df_keller = df_combined.melt(id_vars=['model'], var_name='descritpor')
     # g1.despine(left=True)
     # g1.set_axis_labels("", title)
@@ -231,7 +232,7 @@ def combine_visualize(df1, df2, df3, tasks, ax, title, abs=False, figure_name="d
     g1 = sns.barplot(
         data=melted_df_keller,
         x="descritpor", y="value", hue="model",
-        errorbar="se", ax=ax, palette=['#4d79a4', '#ecc947', '#b07aa0'], linewidth=7)
+        errorbar="se", ax=ax, palette=['#4d79a4', '#ecc947', '#b07aa0','#103961'], linewidth=7)
     g1.set(xlabel='Model', ylabel=title)
     g1.spines['top'].set_visible(False)
     g1.spines['right'].set_visible(False)
@@ -277,20 +278,28 @@ def combine_visualize_separate(df1, df2, df3, tasks, ax, title, type="corr", fig
     # g2.legend.set_title("")
     # g.set_xticklabels(tasks, rotation=45)
     # g2.figure.savefig(figure_name+".pdf")
-def post_process_dataframe(corrss, msess, df_cor_pom, df_cor_alva, df_mse_pom, df_mse_alva, tasks,
+def post_process_dataframe(corrss, msess,corrss_finetuned, msess_finetuned, df_cor_pom, df_cor_alva, df_mse_pom, df_mse_alva, tasks,
                            figure_name="def"):
     plt.rcParams["font.size"] = 40
-    corrss_1_12 = corrss.loc[((corrss["layer"] == 0) | (corrss["layer"] == 12)) & (corrss["model"] == "molformer")]
-    del corrss_1_12["model"]
-    melted_corrss_1_12 = corrss_1_12.melt(id_vars=['layer'], var_name='descritpor')
-    melted_corrss_filtered_increasing = melted_corrss_1_12.groupby('descritpor').filter(
-        lambda x: x.loc[x['layer'] == 12, 'value'].abs().mean() > x.loc[x['layer'] == 0, 'value'].abs().mean())
-    melted_corrss_filtered_decreasing = melted_corrss_1_12.groupby('descritpor').filter(
-        lambda x: x.loc[x['layer'] == 0, 'value'].abs().mean() > x.loc[x['layer'] == 12, 'value'].abs().mean())
-    print(melted_corrss_1_12.descritpor.unique())
-    melted_corrss_filtered_increasing['trend'] = 'Increasing'
-    melted_corrss_filtered_decreasing['trend'] = 'Decreasing'
-    melted_corrss_filtered = pd.concat((melted_corrss_filtered_increasing, melted_corrss_filtered_decreasing))
+
+    # corrss_1_12 = corrss.loc[((corrss["layer"] == 0) | (corrss["layer"] == 12)) & (corrss["model"] == "molformer")]
+    # del corrss_1_12["model"]
+    # melted_corrss_1_12 = corrss_1_12.melt(id_vars=['layer'], var_name='descritpor')
+    # melted_corrss_filtered_increasing = melted_corrss_1_12.groupby('descritpor').filter(
+    #     lambda x: x.loc[x['layer'] == 12, 'value'].abs().mean() > x.loc[x['layer'] == 0, 'value'].abs().mean())
+    # melted_corrss_filtered_decreasing = melted_corrss_1_12.groupby('descritpor').filter(
+    #     lambda x: x.loc[x['layer'] == 0, 'value'].abs().mean() > x.loc[x['layer'] == 12, 'value'].abs().mean())
+    # print(melted_corrss_1_12.descritpor.unique())
+    # melted_corrss_filtered_increasing['trend'] = 'Increasing'
+    # melted_corrss_filtered_decreasing['trend'] = 'Decreasing'
+    # melted_corrss_filtered = pd.concat((melted_corrss_filtered_increasing, melted_corrss_filtered_decreasing))
+
+
+
+
+
+
+
     # fig, ax = plt.subplots(nrows=1,ncols=2,figsize=(30,10))
     # sns.lineplot(
     # data=melted_corrss_filtered_increasing, x="layer", y="value", hue="descritpor", err_style='bars',ax=ax[0]
@@ -301,16 +310,16 @@ def post_process_dataframe(corrss, msess, df_cor_pom, df_cor_alva, df_mse_pom, d
     # f1, ax_agg = plt.subplots(1, 2,figsize=(20, 5))
     f2, ax = plt.subplots(2, 1, figsize=(22, 22))
     # combine_visualize_separate(corrss.loc[corrss["layer"]==12,].iloc[:,corrss.columns != 'layer'], df_cor_pom,df_cor_alva,tasks,ax_agg[0],'Correlation Coefficient',figure_name="Correlation_Avg_"+figure_name)
-    g1 = combine_visualize(corrss.loc[corrss["layer"] == 12].iloc[:, corrss.columns != 'layer'], df_cor_pom,
+    g1 = combine_visualize(corrss.loc[corrss["layer"] == 12].iloc[:, corrss.columns != 'layer'],corrss_finetuned.loc[corrss_finetuned["layer"] == 12].iloc[:, corrss_finetuned.columns != 'layer'], df_cor_pom,
                            df_cor_alva, tasks, ax[0], 'Correlation Coefficient',
                            figure_name="Correlation_" + figure_name,abs=True)
 
     # combine_visualize_separate(msess.loc[msess["layer"]==12].iloc[:,msess.columns != 'layer'], df_mse_pom,df_mse_alva,tasks,ax_agg[1],'MSE',type="mse",figure_name="MSE_Avg_"+figure_name)
-    g2 = combine_visualize(msess.loc[msess["layer"] == 12].iloc[:, msess.columns != 'layer'], df_mse_pom,
+    g2 = combine_visualize(msess.loc[msess["layer"] == 12].iloc[:, msess.columns != 'layer'], msess_finetuned.loc[msess_finetuned["layer"] == 12].iloc[:, msess_finetuned.columns != 'layer'], df_mse_pom,
                            df_mse_alva, tasks, ax[1], 'NRMSE', figure_name="MSE__" + figure_name)
     vertical_plot_tweak(f2, figure_name, g1, g2)
 
-    return melted_corrss_filtered
+    # return melted_corrss_filtered
 
 
 def vertical_plot_tweak(f2, figure_name, g1, g2):
